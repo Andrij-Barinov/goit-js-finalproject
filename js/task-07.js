ba7cb07822fdbdd1d478165feb270f56040df173
Andrii Barinov <140857376+Andrij-Barinov@users.noreply.github.com>
2023-12-22 22:22:36 +0100

    finished finalproject

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const fontSizeControl = document.getElementById('font-size-control');
-        const text = document.getElementById('text');
-        function updateFontSize() {
-            text.style.fontSize = fontSizeControl.value + 'px';
-        }
-        fontSizeControl.addEventListener('input', updateFontSize);
-        updateFontSize();
