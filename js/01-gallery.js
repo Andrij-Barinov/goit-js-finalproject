758e55a490029f9405a37fefd07b4b246f503d63
Andrii Barinov <140857376+Andrij-Barinov@users.noreply.github.com>
2023-12-24 00:10:34 +0100

    finished finalproject

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+  })
+  .join('');
+galleryContainer.innerHTML = galleryMarkup;
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const largeImageSrc = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${largeImageSrc}" width="800" height="600">
+  `);
+  instance.show();
+  window.addEventListener('keydown', onEscKeyPress);
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onEscKeyPress);
+    }
+  }
+});
